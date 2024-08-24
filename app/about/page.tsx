7f0c8c4c7597e61a16e2e8533933dc9f42e2972f
beetho <yvensb002@gmail.com>
2024-08-24 11:38:00 -0400

    les pages sont responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,35 +1,28 @@
-import { Navigation }  from "../components/nav";
+import { Navigation } from "../components/nav";
 import Particles from "../components/particles";
 
 export default function About() {
-
-    return (
-        <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-blue via-zinc-600/70 to-black">
-       
-            <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={50}
-      />
-            <Navigation/>
-            <h1 className="lg:text-xl font-medium duration-150 xl:text-3xl text-white font-display text-center">
-               Hello World... Let's Ride Together !! <br/>
-            </h1>
-            <p className="lg:text-base font-normal xl:text-lg text-white  font-display text-left  max-w-4xl mx-4 lg:mx-8 text-left">
-            Je possède -X- heures de formation et de pratique intensive en programmation orientée objet et en conception 
-            de bases de données relationnelles et NoSQL. J'ai également une expérience approfondie en vente, marketing, 
-            gestion de personnel et design web, ce qui me permet de combiner créativité et stratégie commerciale, notamment 
-            dans le développement d'applications mobiles et web. Je souhaite utiliser mes compétences pour explorer de nouvelles 
-            approches alignées avec mes objectifs de carrière.
-            </p>
-            <p>--------------------------------------------------------------------------</p>
-            <p className="lg:text-base font-normal xl:text-lg text-white  font-display text-left  max-w-4xl mx-4 lg:mx-8 text-left">
-             I have -X- hours of training and intensive practice in object-oriented programming and in designing 
-            relational and NoSQL databases. I also have extensive experience in sales, marketing, personnel management,
-            and web design, which allows me to combine creativity and business strategy, particularly in the development
-            of mobile and web applications. 
-            I wish to use my skills to explore new approaches aligned with my career goals."  
-            </p>
-        </div>
-        
-    );
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-blue via-zinc-600/70 to-black px-4 sm:px-8 md:px-16">
+      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={50} />
+      <Navigation />
+      <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-medium duration-150 text-white font-display text-center my-4 sm:my-8">
+        Hello World... Let's Ride Together !! <br />
+      </h1>
+      <p className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-white font-display text-left max-w-4xl mx-4 lg:mx-8 my-4">
+        Je possède -X- heures de formation et de pratique intensive en programmation orientée objet et en conception
+         de bases de données relationnelles et NoSQL. J'ai également une expérience approfondie en vente, marketing, 
+         gestion de personnel et design web, ce qui me permet de combiner créativité et stratégie commerciale, notamment 
+         dans le développement d'applications mobiles et web. Je souhaite utiliser mes compétences pour explorer de nouvelles 
+         approches alignées avec mes objectifs de carrière.
+      </p>
+      <p className="text-center text-white my-4">----------------------------------------------------------</p>
+      <p className="text-sm sm:text-base md:text-lg lg:text-xl font-normal text-white font-display text-left max-w-4xl mx-4 lg:mx-8 my-4">
+        I have -X- hours of training and intensive practice in object-oriented programming and in designing relational and NoSQL 
+        databases. I also have extensive experience in sales, marketing, personnel management, and web design, which allows me to combine 
+        creativity and business strategy, particularly in the development of mobile and web applications. I wish to use my skills 
+        to explore new approaches aligned with my career goals.
+      </p>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,17 @@ const navigation = [
   { name: "Contact", href: "/contact" },
   { name: "About", href: "/about" },
 ];
-/*
-tout les pages sont dans le dossier app directement. Il ne sont pas dans le dossier Pages
-*/
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-blue via-zinc-600/70 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-x-40">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-blue via-zinc-600/70 to-black px-4 sm:px-8 md:px-16">
+      <nav className="my-8 md:my-16 animate-fade-in">
+        <ul className="flex flex-col sm:flex-row items-center justify-center gap-y-4 sm:gap-x-10 md:gap-x-20 lg:gap-x-40">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-xl duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-lg sm:text-xl duration-500 text-zinc-500 hover:text-zinc-300"
             >
               {item.name}
             </Link>
@@ -31,18 +29,15 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-    <h1 className="py-3.5 px-0.5 z-10 text-4xl bg-gradient-to-r from-red-600 via-orange-600 to-yellow-400 bg-clip-text text-transparent duration-1000 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap">
-  Yvens-- Portfolio
-</h1>
-
-
+      <h1 className="py-2.5 px-0.5 z-10 text-3xl sm:text-5xl md:text-7xl lg:text-9xl bg-gradient-to-r from-red-600 via-orange-600 to-yellow-400 bg-clip-text text-transparent duration-1000 cursor-default text-edge-outline animate-title font-display whitespace-nowrap">
+        Yvens-- Portfolio
+      </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-3xl text-zinc-500 ">
-          Welcome in my Portfolio. If any questions, Feel free to ask. Stay tuned For More!!
+      <div className="my-8 md:my-16 text-center animate-fade-in px-4 sm:px-8">
+        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-zinc-500">
+          Welcome to my Portfolio. If any questions, feel free to ask. Stay tuned for more!!
         </h2>
       </div>
     </div>
   );
-
 }
